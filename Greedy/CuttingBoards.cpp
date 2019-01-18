8cc62ad69f233eeb90fca4b3545352ed5120865c
Rupesh Bordiya <rbordiya22@hotmail.com>
2019-01-18 17:39:44 +0530

    new solution

---
+++ Greedy/CuttingBoards.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+vector<string> split_string(string);
+bool comp(int a,int b)
+{
+    return a>b ;
+}
+// Complete the boardCutting function below.
+int boardCutting(vector<int> cost_y, vector<int> cost_x) {
+    int n=cost_x.size(),m=cost_y.size(),i ;
+   sort(cost_x.begin(),cost_x.end(),comp) ; sort(cost_y.begin(),cost_y.end(),comp) ;
+   int l=0,r=0 ;
+   ll ans=0 ;
+    while(l<n && r<m)
+    {
+       if(cost_x[l]>=cost_y[r])
+       {
+             ans+= (ll)(r+1) * (ll)cost_x[l];
+             l++ ;
+       }
+       else if(cost_x[l]<cost_y[r])
+       {
+             ans+= (ll)(l+1) * (ll)cost_y[r] ;
+             r++ ;
+       }
+    }
+    while(l<n)
+    {
+                ans+= (ll)(m+1) * (ll)cost_x[l];
+                l++ ;
+    }
+    while(r<m)
+    {
+                ans+= (ll)(n+1) * (ll)cost_y[r] ;
+                r++ ;
+    }
+    ans%=1000000007 ;
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string mn_temp;
+        getline(cin, mn_temp);
+
+        vector<string> mn = split_string(mn_temp);
+
+        int m = stoi(mn[0]);
+
+        int n = stoi(mn[1]);
+
+        string cost_y_temp_temp;
+        getline(cin, cost_y_temp_temp);
+
+        vector<string> cost_y_temp = split_string(cost_y_temp_temp);
+
+        vector<int> cost_y(m-1);
+
+        for (int i = 0; i < m-1; i++) {
+            int cost_y_item = stoi(cost_y_temp[i]);
+
+            cost_y[i] = cost_y_item;
+        }
+
+        string cost_x_temp_temp;
+        getline(cin, cost_x_temp_temp);
+
+        vector<string> cost_x_temp = split_string(cost_x_temp_temp);
+
+        vector<int> cost_x(n-1);
+
+        for (int i = 0; i < n-1; i++) {
+            int cost_x_item = stoi(cost_x_temp[i]);
+
+            cost_x[i] = cost_x_item;
+        }
+
+        int result = boardCutting(cost_y, cost_x);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
