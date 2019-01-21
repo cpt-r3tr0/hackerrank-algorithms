7b7456bc8babb49fc8c74a68eccf743bbb2550bd
Rupesh Bordiya <rbordiya22@hotmail.com>
2019-01-21 14:25:01 +0530

    new solution

---
+++ Dynamic Programming/MaximumSubArray.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+const int maxAllowed = 100000;
+
+vector<int>
+maxSumSubArrayContiguous(const vector<int> & numbers)
+{
+    vector<int> ret(2);
+    int sum = 0, maxSum, maxSumNC = 0;
+    vector<int>::size_type i = 0;
+    int allNegative = 1, allNegativeMin = numbers[0];
+
+    maxSum = numbers[i];
+    for (i = 0; i != numbers.size(); ++i) {
+        sum += numbers[i];
+        if (sum < 0) sum = 0;
+        else if (sum > maxSum) maxSum = sum;
+        if (numbers[i] >= 0) {
+            allNegative = 0;
+            maxSumNC += numbers[i];
+        } else {
+            if (allNegativeMin < numbers[i]) allNegativeMin = numbers[i];
+        }
+    }
+    if (allNegative) {
+        ret[0] = allNegativeMin;
+        ret[1] = allNegativeMin;
+    } else {
+        ret[0] = maxSum;
+        ret[1] = maxSumNC;
+    }
+    return ret;
+}
+
+int main(void)
+{
+    int numTestCases;
+    int N;
+
+    cin >> numTestCases;
+    while (numTestCases--) {
+        cin >> N;
+        vector<int> numbers;
+        numbers.clear();
+        while (N--) {
+            int x;
+            cin >> x;
+            if (x > maxAllowed) {
+                cout << "Invalid Input" << endl;
+                break;
+            }
+            numbers.push_back(x);
+        }
+        vector<int> ret;
+        ret = maxSumSubArrayContiguous(numbers);
+        cout << ret[0] << " " << ret[1] << endl;
+    }
+    return 0;
+}
